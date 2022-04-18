5838bc66d2431399f762abea4000fadbf34e9540
rsakml <rossaakmalia66@gmail.com>
2022-04-18 10:25:24 +0700

    Membuat Fitur Export PDF

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,5 @@ Route::get('search', [MahasiswaController::class,'search'])->name('search');
 Route::get('/mahasiswa/nilai/{mahasiswa}', [MahasiswaController::class, 'mhsMatkul'])->name('nilai');
 
 Route::resource('articles', ArticleController::class);
+
+Route::get('/article/cetak_pdf', [ArticleController::class, 'cetak_pdf']);
